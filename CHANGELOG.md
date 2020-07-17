f514ddec315c01b52fc659b1009d812615818c7e
George Birnbaum <gb@glamorise.com>
2020-07-17 16:40:46 -0400

    add temperature conversion

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `Unit Conversions` will be documented in this file
 
-## 1.0.0 - 202X-XX-XX
+## 1.0.0 - 2020-07-17
 
 - initial release
+
+## 1.1.0 - 2020-07-17
+
+- Add Temperature conversion
---
+++ tests/TemperatureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Georanma\UnitConversions\Tests;
+
+use Georanma\UnitConversions\Temperature;
+use PHPUnit\Framework\TestCase;
+
+class TemperatureTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_celsius_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::fromCelsius(100)->toFahrenheit();
+        $this->assertEquals(212, $fahrenheit);
+    }
+}
